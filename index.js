88040bb5f7c40e9fc37dc2b581940ef4aad0c67c
Ouail-01 <ouaillaarabi@gmail.com>
2023-01-04 01:07:02 +0100

    Add the object variable

--- index.js
+++ index.js
@@ -12,3 +12,55 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   navMenu.classList.remove('active');
   body.classList.remove('active');
 }));
+
+// Dynamic Featured speakers
+
+const speakers = [
+  {
+    name: 'Florentino Pérez',
+    academyWork: 'Director',
+    description: `75 years old <br>Spanish businessman, civil engineer, former politician, and the current 
+    president of Real Madrid as well as Chairman and CEO.`,
+    speakerImage: 'img/Florentino_perez.jpg',
+  },
+
+  {
+    name: 'José Mourinho',
+    academyWork: 'Coach',
+    description: `59 years old <br>Once dubbed "The Special One" by the British media,
+    Mourinho is widely considered to be among the greatest.`,
+    speakerImage: 'img/jose-mourinho-1.jpeg',
+  },
+
+  {
+    name: 'Pep Guardiola',
+    academyWork: 'Assistant Coach',
+    description: `51 years old <br>Spanish professional football manager and former player, who is the current
+    manager of Premier League club Manchester City.`,
+    speakerImage: 'img/pep-guardiola.jpeg',
+  },
+
+  {
+    name: 'Zinedine Zidane',
+    academyWork: 'Assistant Coach',
+    description: `50 years old <br>French professional football manager and former player who played as an
+    attacking midfielder.`,
+    speakerImage: 'img/Zinedine_Zidane.jpeg',
+  },
+
+  {
+    name: 'Grégory Dupont',
+    academyWork: 'Fitness Coach',
+    description: `50 years old <br>Sports physical trainer, he is known as the scientist since he usually plans
+    his workouts based on scientific research.`,
+    speakerImage: 'img/gregorgy-dupont.jpeg',
+  },
+
+  {
+    name: 'Grégory Dupont',
+    academyWork: 'Athletic Coach',
+    description: `59 years old <br>Italien physical trainer, known as "the sergeant" for the harshness of his
+    methods,`,
+    speakerImage: 'img/antonio-pintus.jpg',
+  },
+];
